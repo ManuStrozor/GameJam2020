fedb2611b0d9399f2dc0ae0722a1339afe47dc43
LeDernierDeNous <mouloud3826@gmail.com>
2020-02-04 17:21:14 +0100

    Mise en forme du texte du menu

--- screen.py
+++ screen.py
@@ -10,35 +10,36 @@ font_titre = pygame.font.SysFont('comicsans', 85, True)
 font = pygame.font.SysFont('comicsans', 40, True)
 
 GREY = (220, 220, 220)
-size = (500, 80)
+size = (512, 80)
 
 bouton_play = pygame.Surface(size)
 rect_play = bouton_play.get_rect()
 rect_play.x = 256
 rect_play.y = 250
-pygame.draw.rect(win, GREY, rect_play)
 
 bouton_cred = pygame.Surface(size)
 rect_cred = bouton_cred.get_rect()
 rect_cred.x = 256
 rect_cred.y = 350
-pygame.draw.rect(win, GREY, rect_cred)
 
 bouton_help = pygame.Surface(size)
 rect_help = bouton_help.get_rect()
 rect_help.x = 256
 rect_help.y = 450
-pygame.draw.rect(win, GREY, rect_help)
 
 bouton_quit = pygame.Surface(size)
 rect_quit = bouton_quit.get_rect()
 rect_quit.x = 256
 rect_quit.y = 550
-pygame.draw.rect(win, GREY, rect_quit)
 
 TEXT = 'Rien..'
 
 def draw_menu():
+    # Dessins
+    pygame.draw.rect(win, GREY, rect_play)
+    pygame.draw.rect(win, GREY, rect_cred)
+    pygame.draw.rect(win, GREY, rect_help)
+    pygame.draw.rect(win, GREY, rect_quit)
     # Textes
     win.blit(pygame.image.load('assets/background.png'), (0, 0))
     text_menu = font_titre.render('Menu', 1, (255, 0, 255))
@@ -49,16 +50,18 @@ def draw_menu():
     text_survol = font.render(TEXT, 1, (255, 0, 255))
 
     # Blit
-    win.blit(text_menu, (420, 130))
+    win.blit(text_menu, (512-text_menu.get_rect().centerx, 130))
     win.blit(bouton_play, (256, 250))
-    win.blit(text_play, (256, 250))
+    win.blit(text_play, (512-text_play.get_rect().centerx, 250+bouton_play.get_rect().centery-15))
     win.blit(bouton_cred, (256, 350))
-    win.blit(text_cred, (256, 350))
+    win.blit(text_cred, (512-text_cred.get_rect().centerx, 350+bouton_cred.get_rect().centery-15))
     win.blit(bouton_help, (256, 450))
-    win.blit(text_help, (256, 450))
+    win.blit(text_help, (512-text_help.get_rect().centerx, 450+bouton_help.get_rect().centery-15))
     win.blit(bouton_quit, (256, 550))
-    win.blit(text_quit, (256, 550))
+    win.blit(text_quit, (512-text_quit.get_rect().centerx, 550+bouton_quit.get_rect().centery-15))
     win.blit(text_survol, (256, 650))
+
+    gerer_event()
 
 def gerer_event():
     global TEXT
@@ -71,7 +74,7 @@ def gerer_event():
         over_help = rect_help.collidepoint(x, y)
         over_quit = rect_quit.collidepoint(x, y)
         if over_play:
-            TEXT = 'Je veux jouer'
+            TEXT = 'Commencer a jouer'
         elif over_cred:
             TEXT = 'Voir les crédits'
         elif over_help:
@@ -92,8 +95,6 @@ while True:
             quit()
 
     draw_menu()
-    gerer_event()
-    pygame.display.flip()
     pygame.display.update()
 
 pygame.quit()
